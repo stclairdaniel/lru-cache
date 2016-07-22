f25bc85d3c2594f3c95460261f232103f70121a5
App Academy Student <student@appacademy.io>
2016-07-22 15:40:52 -0700

    Implement linked list, passed all specs'

--- skeleton/lib/p04_linked_list.rb
+++ skeleton/lib/p04_linked_list.rb
@@ -1,3 +1,5 @@
+require 'byebug'
+
 class Link
   attr_accessor :key, :val, :next, :prev
 
@@ -14,7 +16,15 @@ class Link
 end
 
 class LinkedList
+  include Enumerable
+
+  attr_reader :head, :tail
+
   def initialize
+    @head = Link.new
+    @tail = Link.new
+    @head.next = @tail
+    @tail.prev = @head
   end
 
   def [](i)
@@ -23,31 +33,65 @@ class LinkedList
   end
 
   def first
+    @head.next
   end
 
   def last
+    @tail.prev
   end
 
   def empty?
+    @head.next == @tail
   end
 
   def get(key)
+    check_link = @head.next
+    check_key = check_link.key
+    until check_key == key
+      break if check_link == @tail
+      check_link = check_link.next
+      check_key = check_link.key
+    end
+    check_link.val
   end
 
   def include?(key)
+    self.each do |link|
+      return true if link.key == key
+    end
+    false
   end
 
   def insert(key, val)
+    unless self.include?(key)
+      new_link = Link.new(key, val)
+      @tail.prev.next = new_link
+      new_link.prev = @tail.prev
+      new_link.next = @tail
+      @tail.prev= new_link
+    end
   end
 
   def remove(key)
+    self.each do |link|
+      if link.key == key
+        link.prev.next = link.next
+        link.next.prev = link.prev
+        return true
+      end
+    end
+    false
   end
 
   def each
+    current_link = @head
+    until current_link == @tail.prev
+      current_link = current_link.next
+      yield(current_link)
+    end
   end
 
-  # uncomment when you have `each` working and `Enumerable` included
-  # def to_s
-  #   inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
-  # end
+  def to_s
+    inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
+  end
 end
