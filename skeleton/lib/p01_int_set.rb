46ff379fcbc7158faa773240f9ad72a3467c7c3a
App Academy Student <student@appacademy.io>
2016-07-22 10:46:46 -0700

    Finish MaxIntSet

--- skeleton/lib/p01_int_set.rb
+++ skeleton/lib/p01_int_set.rb
@@ -1,23 +1,28 @@
 class MaxIntSet
   def initialize(max)
-    @store = Array.new(max, false)
+    @max = max
+    @store = Array.new(max) { false }
   end
 
   def insert(num)
     validate!(num)
+    @store[num] = true
   end
 
   def remove(num)
     validate!(num)
+    @store[num] = false
   end
 
   def include?(num)
     validate!(num)
+    @store[num] == true
   end
 
   private
 
   def is_valid?(num)
+    num.between?(0, @max)
   end
 
   def validate!(num)
