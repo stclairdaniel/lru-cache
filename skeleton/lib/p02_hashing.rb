e7056ec7973c262600623572a6f2652d73f4e2b7
App Academy Student <student@appacademy.io>
2016-07-22 15:37:52 -0700

    Finish Hashing function - naiive implementation

--- skeleton/lib/p02_hashing.rb
+++ skeleton/lib/p02_hashing.rb
@@ -4,11 +4,30 @@ end
 
 class Array
   def hash
+    hash_string = ''
+    hash_concat = ''
+    count = 1
+    self.each do |el|
+      if el.is_a?(Numeric)
+        hash_concat.concat(el.hash.to_s)
+        count += 1
+      elsif el.is_a?(Array)
+        hash_concat.concat(el.hash)
+        count += 1
+      else
+        el.is_a?(Symbol) ? el.to_s : el
+        hash_concat.concat(el.hash.to_s)
+        count += 1
+      end
+    end
+    hash_arr = hash_concat.chars
+    hash_arr.select.with_index { |char, idx| idx % count == 0 }.join.to_i
   end
 end
 
 class String
   def hash
+    self.chars.map(&:ord).hash
   end
 end
 
@@ -16,6 +35,6 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    self.keys.sort.concat(self.values.sort).hash
   end
 end
