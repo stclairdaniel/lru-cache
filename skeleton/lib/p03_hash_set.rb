871740df738f2615e65cb0c2b27d03471b3c1af4
App Academy Student <student@appacademy.io>
2016-07-22 15:38:44 -0700

    Implement Hash_Set, using Hashes as location values

--- skeleton/lib/p03_hash_set.rb
+++ skeleton/lib/p03_hash_set.rb
@@ -9,18 +9,25 @@ class HashSet
   end
 
   def insert(key)
+    resize! unless @count < num_buckets
+    unless self.include?(key)
+      self[key.hash] << key
+      @count += 1
+    end
   end
 
   def include?(key)
+    self[key.hash].any? { |el| el == key }
   end
 
   def remove(key)
+    @count -= 1 if self[key.hash].delete(key)
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -28,5 +35,9 @@ class HashSet
   end
 
   def resize!
+    elements = @store.flatten
+    @store = Array.new(num_buckets * 2) { Array.new }
+    elements.each { |el| self.insert(el) }
   end
+
 end
