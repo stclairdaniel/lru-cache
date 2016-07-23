f9da90278e159fd066aacd34ec40a43b8cbed054
App Academy Student <student@appacademy.io>
2016-07-22 17:18:42 -0700

    Started LRU cache, no passing specs

--- skeleton/lib/p06_lru_cache.rb
+++ skeleton/lib/p06_lru_cache.rb
@@ -14,7 +14,13 @@ class LRUCache
     @map.count
   end
 
-  def get(key)
+  def get(key, &prc)
+    unless @store.include?(key)
+      calc!(key, &prc)
+    else
+      update_link!(@store[key])
+    end
+    eject! unless count < @max
   end
 
   def to_s
@@ -23,14 +29,23 @@ class LRUCache
 
   private
 
-  def calc!(key)
+  def calc!(key, &prc)
     # suggested helper method; insert an (un-cached) key
+    unless @store.include?(key)
+      # block_given?  ? yielded = prc.call(key) : @store.get(key)
+      @store.insert(key, yield(key))
+      @map[key] = yield(key)
+    end
   end
 
-  def update_link!(link)
-    # suggested helper method; move a link to the end of the list
+  def update_link!(key)
+    @store.remove(key)
+    @store.insert(key, @store.get(key))
   end
 
   def eject!
+    lru_key = @store.head.next.key
+    @store.remove(lru_key)
+    @map.delete(lru_key)
   end
 end
